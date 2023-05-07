50d5de6ffe0dea176905e3399360e3dac8b3ccc6
nxhdev2002 <xuanhoangv7c@gmail.com>
2023-05-07 08:50:38 +0700

    change login page layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@include('layouts.header')
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
+<p>Trang chu</p>
 
-<body></body>
-
-</html>
+@include('layouts.footer')
