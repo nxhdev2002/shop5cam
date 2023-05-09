c2d0aecc37f12377fcd885a66f68d00f07556e6e
huuminhvu <78521987+huuminhvu@users.noreply.github.com>
2023-05-09 22:55:15 +0700

    update model  (#7)
    * homepage
    
    * homepage
    
    * homepage đổ data
    
    * model

--- app/Models/Ads.php
+++ app/Models/Ads.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Ads extends Model
 {
     use HasFactory;
+    function products(){ 
+        return $this->belongsTo(Product::class,'product_id','id');
+    }
+    function users(){ 
+        return $this->belongsTo(User::class,'user_id','id');
+    }
 }
