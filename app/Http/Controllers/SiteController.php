22f78a513d971109febdb6ed1781fb63dd31f9ac
nxhdev2002 <xuanhoangv7c@gmail.com>
2023-05-09 13:29:31 +0700

    fix header & fix some bug at controller

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -12,36 +13,11 @@ class SiteController extends Controller
 {
     public function index()
     {
-
-        // $users = DB::table('products')->get();
-        // // list 10 san pham dau tien
-        // $products_10first = DB::table('products')->take(10)->get();
-        // // list pro theo số lg bán
-        // $products_quantity = Product::select('name', DB::raw('SUM(quantity) as total'))
-        // ->join('transactions', 'products.id', '=', 'transactions.product_id')
-        // ->groupBy('products.id')
-        // ->orderBy('total', 'desc')
-        // ->take(10)
-        // ->get();
-        // $product1 = Product::where('amount', '>', '700')->get();
-        // // list pro re nhat
-        // $products_cheap = Product::orderBy('price', 'asc')
-        // ->take(10)
-        // ->get();
-        // // list pro dat nhat
-        // $products_expensive = Product::orderBy('price', 'desc')
-        // ->take(10)
-        // ->get();
-        // // list pro theo ngày up
-        // $products_datecreated = Product::select('products.*')
-        // // ->join('order_items', 'products.id', '=', 'order_items.product_id')
-        // ->orderBy('products.created_at', 'desc')
-        // ->groupBy('products.id')
-        // ->take(10)
-        // ->get();
         $title = "Trang chủ";
+        $categories = Category::all();
         return view('index', compact(
-            'title'
+            'title',
+            'categories'
         ));
         //list pro theo tên danh mục  
     }
---
+++ resources/views/components/home/services.blade.php
@@ -0,0 +1,78 @@
+<section class="services">
+    <div class="container mx-auto my-3 bg-white">
+        <div class="flex flex-col items-center mx-3 my-3 basis-full md:basis-1/4">
+            <h1 class="p-4 text-lg font-medium">Dịch vụ & Điểm mạnh</h1>
+            <p class="font-light text-gray-400">Own services & strengths</p>
+        </div>
+
+        <div class="grid grid-cols-2 gap-6 p-4 md:grid-cols-4">
+            <div class="relative p-4">
+                <div class="h-3/4">
+                    <img src="https://blog.dktcdn.net/files/4-cach-de-dan-dau-xu-the-ban-le.jpg"
+                        class="w-full h-full max-w-full max-h-">
+                </div>
+                <h2 class="left-0 w-full pt-2 font-light text-center bottom-2">Uy tín tin cậy</h2>
+                <p class="text-xs text-center font-extralight">Hệ thống giữ tiền người bán trong thời gian 7 ngày. Nếu
+                    có bất cứ sai
+                    sót nào xảy ra, hệ thống sẽ hoàn lại tiền người mua</p>
+                <div class="absolute p-2 bg-blue-500 rounded-full right-3 top-2/3 bg-opacity-40">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path
+                            d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                    </svg>
+                </div>
+            </div>
+
+            <div class="relative p-4">
+                <div class="h-3/4">
+                    <img src="https://cdnimg.vietnamplus.vn/uploaded/znaets/2022_10_17/pr_plx_21.jpg"
+                        class="w-full h-full max-w-full max-h-">
+                </div>
+                <h2 class="left-0 w-full pt-2 font-light text-center bottom-2">Giao dịch dễ dàng</h2>
+                <p class="text-xs text-center font-extralight">Hỗ trợ nhiều phương thức thanh toán như Visa, Mastercard,
+                    Chuyển khoản ngân hàng, Momo,...</p>
+                <div class="absolute p-2 bg-blue-500 rounded-full right-3 top-2/3 bg-opacity-40">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path fill-rule="evenodd"
+                            d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </div>
+            </div>
+
+            <div class="relative p-4 ">
+                <div class="h-3/4">
+                    <img src="https://lh6.googleusercontent.com/cjfg7nMb9kLNq8aG5bBWqUu2XydrNOCE2BeLqvYt_YZ-lp0UafmC9FfysXhO5mFbzi7BNKeHwMZxZKC9uktC38QgRaKnN3v0tU1506C7oPGBFwWBjeuuM2IkqaCbGWllQ1Dwzc2o"
+                        class="w-full h-full max-w-full max-h-">
+                </div>
+                <h2 class="left-0 w-full pt-2 font-light text-center bottom-2">Đánh giá giao dịch</h2>
+                <p class="text-xs text-center font-extralight">Hệ thống đánh giá giao dịch, quý khách có thể xem đánh
+                    giá để quyết định có chọn mặt hàng đó hay không.</p>
+                <div class="absolute p-2 bg-blue-500 rounded-full right-3 top-2/3 bg-opacity-40">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path fill-rule="evenodd"
+                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </div>
+            </div>
+
+            <div class="relative p-4">
+                <div class="h-3/4">
+                    <img src="https://lumi.vn/wp-content/uploads/2020/10/Internet-of-thing.jpg"
+                        class="w-full h-full max-w-full max-h-">
+                </div>
+                <h2 class="left-0 w-full pt-2 font-light text-center bottom-2">Hệ thống thông minh</h2>
+                <p class="text-xs text-center font-extralight">Hệ thống trang web đánh giá mặt hàng dựa trên tương tác
+                    và sẽ gợi ý mặt hàng trên trang chủ.</p>
+                <div class="absolute p-3 bg-blue-500 rounded-full right-3 top-2/3 bg-opacity-40">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path fill-rule="evenodd"
+                            d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
