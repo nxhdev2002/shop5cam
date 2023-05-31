ebc25d911daca55fa4a07b6360074cf8714b00e4
nxhdev2002 <xuanhoangv7c@gmail.com>
2023-05-31 16:16:47 +0700

    change site name in admin db

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
-<aside class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0">
+<aside class="z-20 flex-shrink-0 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block">
     <div class="py-4 text-gray-500 dark:text-gray-400">
         <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="/admin/dashboard">
-            Shop5Cam
+            {{env("SITE_NAME")}}
         </a>
         <ul class="mt-6">
             <li class="relative px-6 py-3">
