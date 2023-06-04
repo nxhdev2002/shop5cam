26b4460d379cc57738bdcf62d19cb69d3fc78e25
nxhdev2002 <xuanhoangv7c@gmail.com>
2023-06-05 00:09:50 +0700

    update upload image

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\Order;
+use App\Models\WebConfig;
 use Cloudinary\Cloudinary;
 use Cloudinary\Transformation\Resize;
 
@@ -16,7 +17,7 @@ class SellerProductController extends Controller
     public function createProduct()
     {
         $category = Category::where('status', $category)->get();
-        return view('',compact('category'));
+        return view('', compact('category'));
     }
     public function storeProduct(Request $request)
     {
@@ -32,32 +33,24 @@ class SellerProductController extends Controller
         $product = new Product;
         $product->name = $request->input('name');
         $product->description = $request->input('description');
-	    $user = Auth::user();
-	    $product->seller_id = $user->id;
+        $user = Auth::user();
+        $product->seller_id = $user->id;
         //$category = Category::where('name', $categoryName)->first();
         // nhap danh muc dang text
-	    // $categoryName = $request->input('category');
-	    // $category = Category::findOrCreate(['name' => $categoryName], ['status' => 1]);
-	    // $product->category_id = $category->id;
+        // $categoryName = $request->input('category');
+        // $category = Category::findOrCreate(['name' => $categoryName], ['status' => 1]);
+        // $product->category_id = $category->id;
         $product->category_id = $request->input('category_id');
-        $cloudinary = new Cloudinary(
-            [
-                'cloud' => [
-                    'cloud_name' => 'dhcevvymr',
-                    'api_key'    => '543482997368214',
-                    'api_secret' => '2ThE9oQ7OSNq5q6mVTI4Ajplwog',
-                ],
-            ]
-        );
+        $cloudinary = new Cloudinary(json_decode(WebConfig::getCloudinaryConfig(), true));
 
         $file = $cloudinary->uploadApi()->upload(
             $request->file('thumb')->path(),
             ['public_id' => $request->file('thumb')->getClientOriginalName()]
         );
-	    $product->guarantee = $request->input('guarantee');
-	    $product->price = $request->input('price');
-	    $product->amount = $request->input('amount');	
-	    $product->save();	
+        $product->guarantee = $request->input('guarantee');
+        $product->price = $request->input('price');
+        $product->amount = $request->input('amount');
+        $product->save();
         $productDetail = new ProductDetail();
         $productDetail->product_id = $product->id;
         $productDetail->detail = $request->input('detail');
@@ -66,18 +59,20 @@ class SellerProductController extends Controller
         $productDetail->save();
     }
 
-    public function history(){
-	    $user = Auth::user();
+    public function history()
+    {
+        $user = Auth::user();
         $history = Product::join('orders', 'products.id', '=', 'orders.product_id')
-               ->select('products.*', 'orders.*')
-               ->where('seller_id',$user->id)
-               ->get();
-               return view('', compact('history'));
+            ->select('products.*', 'orders.*')
+            ->where('seller_id', $user->id)
+            ->get();
+        return view('', compact('history'));
     }
 
-    public function inventory(){
+    public function inventory()
+    {
         $user = Auth::user();
-        $inventory = Prouduct::where('seller_id',$user->id);
+        $inventory = Prouduct::where('seller_id', $user->id);
         return view('', compact('inventory'));
-}
+    }
 }
