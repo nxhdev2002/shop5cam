c2b61a9c74f1cba5e30dab5028a34f82fca73f0b
nxhdev2002 <xuanhoangv7c@gmail.com>
2023-07-04 15:36:29 +0700

    update text

--- resources/views/admin/frontend/products/index.blade.php
+++ resources/views/admin/frontend/products/index.blade.php
@@ -3,7 +3,7 @@
 <main class="p-4 sm:ml-64">
     <div class="container grid px-6 mx-auto">
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Deposit Request
+            Products
         </h2>
         <div class="w-full overflow-hidden rounded-lg shadow-xs">
             <div class="w-full overflow-x-auto">
